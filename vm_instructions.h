87c67c3fb85a17e02183bf22839c39dc2b04091e
Tyler Knoop <143290179+Monotude@users.noreply.github.com>
2023-09-30 13:19:16 -0400

    Add files via upload

--- vm_instructions.h
+++ vm_instructions.h
@@ -3,6 +3,8 @@
 
 extern void executeInstruction(bin_instr_t instruction);
 extern void registerInstructionExecute(bin_instr_t instruction);
+extern void sll(bin_instr_t instruction);
+extern void srl(bin_instr_t instruction);
 extern void systemcallInstructionExecute(bin_instr_t instruction);
 extern void immediateInstructionExecute(bin_instr_t instruction);
 extern void jumpInstructionExecute(bin_instr_t instruction);
