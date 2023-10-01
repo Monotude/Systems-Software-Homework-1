750210e0d99782856c22a6f4159664dac6364477
Tyler Knoop <143290179+Monotude@users.noreply.github.com>
2023-09-30 21:53:37 -0400

    Add files via upload

--- vm_instructions.c
+++ vm_instructions.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
 #include "instruction.h"
 #include "machine_types.h"
+#include "main.h"
 #include "utilities.h"
-#include "vm_registers.h"
 #include "vm_memory.h"
+#include "vm_registers.h"
 
 void executeInstruction(bin_instr_t instruction)
 {
@@ -32,251 +34,49 @@ void registerInstructionExecute(bin_instr_t instruction)
 {
     switch (instruction.reg.func)
     {
-      case 33:
-        add(instruction);
-        break;
-      case 35:
-        sub(instruction);
-        break;
-      case 25:
-        mul(instruction); //function needs to be checked
-        break;
-      case 27:
-        div(instruction);
-        break;
-      case 16:
-        mfhi(instruction);
-        break;
-      case 18:
-        mflo(instruction);
-        break;
-      case 36:
-        andInstruction(instruction);
-        break;
-      case 37:
-        bor(instruction);
-        break;
-      case 39:
-        nor(instruction);
-        break;
-      case 38:
-        xorInstruction(instruction);
-        break;
-      case 0:
+    case 0:
         sll(instruction);
         break;
-      case 3:
+    case 3:
         srl(instruction);
         break;
-      case 8:
+    case 8:
         jr(instruction);
         break;
-      case 12:
+    case 12:
         systemcallInstructionExecute(instruction);
         break;
-      
-          
-    }
-}
-
-void systemcallInstructionExecute(bin_instr_t instruction) //looks at opcode
-{
-  
-}
-
-void immediateInstructionExecute(bin_instr_t instruction)
-{
-  switch(instruction.immed.op){
-    case 9:
-      addi(instruction);
-      break;
-    case 12:
-      andi(instruction);
-      break;
-    case 13:
-      bori(instruction);
-      break;
-    case 14:
-      xori(instruction);
-      break;
-    case 4:
-      beq(instruction);
-      break;
-    case 1:
-      bgez(instruction);
-      break;
-    case 7:
-      bgtz(instruction);
-      break;
-    case 6:
-      blez(instruction);
-      break;
-    case 8:
-      bltz(instruction);
-      break;
-    case 5:
-      bne(instruction);
-      break;
-    case 36:
-      lbu(instruction);
-      break;
+    case 16:
+        mfhi(instruction);
+        break;
+    case 18:
+        mflo(instruction);
+        break;
+    case 25:
+        mul(instruction);
+        break;
+    case 27:
+        div(instruction);
+        break;
+    case 33:
+        add(instruction);
+        break;
     case 35:
-      lw(instruction);
-      break;
-    case 40:
-      sb(instruction);
-      break;
-    case 43:
-      sw(instruction);
-      break;
-    
-    
-  }
-}
-
-void jumpInstructionExecute(bin_instr_t instruction)
-{
-  
-}
-
-void addi(bin_instr_t instruction){
-  //Add immediate: GPR[t] ← GPR[s] + sgnExt(i)
-  GPR[instruction.immed.rt] = GPR[instruction.immed.rs] + machine_types_sgnExt(instruction.immed.immed);
-}
-
-void andi(bin_instr_t instruction){
-  //Bitwise And immediate: GPR[t] ← GPR[s] ∧ zeroExt(i)
-  GPR[instruction.immed.rt] = GPR[instruction.immed.rs] & machine_types_zeroExt(instruction.immed.immed);
-}
-
-void bori(bin_instr_t instruction){
-  //Bitwise Or immediate: GPR[t] ← GPR[s] ∨ zeroExt(i)
-  GPR[instruction.immed.rt] = GPR[instruction.immed.rs] | machine_types_zeroExt(instruction.immed.immed);
-}
-
-void xori(bin_instr_t instruction){
-  //Bitwise Xor immediate: GPR[t] ← GPR[s] xor zeroExt(i)
-  GPR[instruction.immed.rt] = GPR[instruction.immed.rs] ^ machine_types_zeroExt(instruction.immed.immed);
-}
-
-void beq(bin_instr_t instruction){
-  //Branch on Equal: if GPR[s] = GPR[t] then PC ← PC + formOffset(o)
-  if(GPR[instruction.immed.rs] == GPR[instruction.immed.rt])
-    PC = PC + machine_types_formOffset(instruction.immed.immed);
-}
-
-void bgez(bin_instr_t instruction){
-  //Branch ≥ 0: if GPR[s] ≥ 0 then PC ← PC + formOffset(o)
-  if(GPR[instruction.immed.rs] >= 0)
-    PC = PC + machine_types_formOffset(instruction.immed.immed);
-}
-
-void bgtz(bin_instr_t instruction){
-  //Branch > 0: if GPR[s] > 0 then PC ← PC + formOffset(o)
-  if(GPR[instruction.immed.rs] > 0)
-    PC = PC + machine_types_formOffset(instruction.immed.immed);
-}
-
-void blez(bin_instr_t instruction){
-  //Branch ≤ 0: if GPR[s] ≤ 0 then PC ← PC + formOffset(o)
-  if(GPR[instruction.immed.rs] <= 0)
-    PC = PC + machine_types_formOffset(instruction.immed.immed);
-}
-
-void bltz(bin_instr_t instruction){
-  //Branch < 0: if GPR[s] < 0 then PC ← PC + formOffset(o)
-  if(GPR[instruction.immed.rs] < 0)
-    PC = PC + machine_types_formOffset(instruction.immed.immed);
-}
-
-void bne(bin_instr_t instruction){
-  //Branch Not Equal: if GPR[s] ̸= GPR[t] then PC ← PC + formOffset(o)
-  if(GPR[instruction.immed.rs] != GPR[instruction.immed.rt])
-    PC = PC + machine_types_formOffset(instruction.immed.immed);
-}
-
-void lbu(bin_instr_t instruction){
-  //Load Byte Unsigned: GPR[t] ← zeroExt(memory[GPR[b] + formOffset(o)])
-  GPR[instruction.immed.rt] = machine_types_zeroExt(Memory.bytes[GPR[instruction.immed.rs] + machine_types_formOffset(instruction.immed.immed)]);
-}
-
-void lw(bin_instr_t instructions){
-  //Load Word (4 bytes): GPR[t] ← memory[GPR[b] + formOffset(o)
-  GPR[instructions.immed.rt] = Memory.words[GPR[instructions.immed.rs] + machine_types_formOffset(instructions.immed.immed)];
-}
-
-void sb(bin_instr_t instructions){
-  //Store Byte (least significant byte of GPR[t]): memory[GPR[b] + formOffset(o)] ← GPR[t]
-  Memory.bytes[GPR[instructions.immed.rs] + machine_types_formOffset(instructions.immed.immed)] = GPR[instructions.immed.rt];
-}
-
-void sw(bin_instr_t instructions){
-  // Store Word (4 bytes): memory[GPR[b] + formOffset(o)] ← GPR[t]
-  Memory.words[GPR[instructions.immed.rs] + machine_types_formOffset(instructions.immed.immed)] = GPR[instructions.immed.rt];
-}
-
-void mfhi(bin_instr_t instruction){
-  //Move from HI: GPR[d] ← HI
-  GPR[instruction.reg.rd] = HI;
-}
-
-void mflo(bin_instr_t instruction){
-  //Move from LO: GPR[d] ← LO
-  GPR[instruction.reg.rd] = LO;
-}
-
-void andInstruction(bin_instr_t instruction){
-  //Bitwise And: GPR[d] ← GPR[s] ∧ GPR[t]
-  GPR[instruction.reg.rd] = GPR[instruction.reg.rs] & GPR[instruction.reg.rt];
-}
-
-void bor(bin_instr_t instruction){
-  //Bitwise Or: GPR[d] ← GPR[s] ∨ GPR[t]
-  GPR[instruction.reg.rd] = GPR[instruction.reg.rs] | GPR[instruction.reg.rt];
-}
-
-void nor(bin_instr_t instruction){
-  //Bitwise Not-Or: GPR[d] ← ¬(GPR[s] ∨ GPR[t])
-  GPR[instruction.reg.rd] = ~(GPR[instruction.reg.rs] | GPR[instruction.reg.rt]);
-}
-
-void xorInstruction(bin_instr_t instruction){
-  //Bitwise Exclusive-Or: GPR[d] ← GPR[s] xor GPR[t]
-  GPR[instruction.reg.rd] = GPR[instruction.reg.rs] ^ GPR[instruction.reg.rt];
-}
-
-void mul(bin_instr_t instruction){ //Needs to be checked********************
-  
-  /*Multiply: Multiply GPR[s] and GPR[t],
-  putting the least significant bits in LO
-  and the most significant bits in HI.
-  (HI, LO) ← GPR[s] × GPR[t]*/
-  long product = GPR[instruction.reg.rs] * GPR[instruction.reg.rt];
-  HI = (product & 0xFFFFFFFF00000000) >> 32;
-  LO = product & 0x00000000FFFFFFFF;
-}
-
-void div(bin_instr_t instruction){
-    /*Divide (remainder in HI, quotient in LO):
-    HI ← GPR[s] % GPR[t]
-    LO ← GPR[s]/GPR[t])*/
-    HI = GPR[instruction.reg.rs] % GPR[instruction.reg.rt];
-    LO = GPR[instruction.reg.rs]/GPR[instruction.reg.rt];
-}
-
-void sub(bin_instr_t instruction){
-  //Subtract: GPR[d] ← GPR[s] − GPR[t]
-  GPR[instruction.reg.rd] = GPR[instruction.reg.rs] - GPR[instruction.reg.rt];
-}
-
-void add(bin_instr_t instruction){
-  //Add: GPR[d] ← GPR[s] + GPR[t]
-  GPR[instruction.reg.rd] = GPR[instruction.reg.rs] + GPR[instruction.reg.rt];
-}
-
-void jr(bin_instr_t instruction){
-  //Jump Register: PC ← GPR[s]
-  PC = GPR[instruction.reg.rs];
+        sub(instruction);
+        break;
+    case 36:
+        andInstruction(instruction);
+        break;
+    case 37:
+        bor(instruction);
+        break;
+    case 38:
+        xorInstruction(instruction);
+        break;
+    case 39:
+        nor(instruction);
+        break;
+    }
 }
 
 void sll(bin_instr_t instruction)
@@ -287,4 +87,275 @@ void sll(bin_instr_t instruction)
 void srl(bin_instr_t instruction)
 {
     GPR[instruction.reg.rd] = GPR[instruction.reg.rt] >> instruction.reg.shift;
+}
+
+void jr(bin_instr_t instruction)
+{
+    PC = GPR[instruction.reg.rs];
+}
+
+void mfhi(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = HI;
+}
+
+void mflo(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = LO;
+}
+
+void mul(bin_instr_t instruction)
+{
+    long product = GPR[instruction.reg.rs] * GPR[instruction.reg.rt];
+    HI = (product & 0xFFFFFFFF00000000) >> 32;
+    LO = product & 0x00000000FFFFFFFF;
+}
+
+void div(bin_instr_t instruction)
+{
+    HI = GPR[instruction.reg.rs] % GPR[instruction.reg.rt];
+    LO = GPR[instruction.reg.rs] / GPR[instruction.reg.rt];
+}
+
+void add(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = GPR[instruction.reg.rs] + GPR[instruction.reg.rt];
+}
+
+void sub(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = GPR[instruction.reg.rs] - GPR[instruction.reg.rt];
+}
+
+void andInstruction(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = GPR[instruction.reg.rs] & GPR[instruction.reg.rt];
+}
+
+void bor(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = GPR[instruction.reg.rs] | GPR[instruction.reg.rt];
+}
+
+void xorInstruction(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = GPR[instruction.reg.rs] ^ GPR[instruction.reg.rt];
+}
+
+void nor(bin_instr_t instruction)
+{
+    GPR[instruction.reg.rd] = ~(GPR[instruction.reg.rs] | GPR[instruction.reg.rt]);
+}
+
+void systemcallInstructionExecute(bin_instr_t instruction)
+{
+    switch (instruction.syscall.code)
+    {
+    case 4:
+    {
+        pstr(instruction);
+        break;
+    }
+    case 10:
+    {
+        exit(0);
+        break;
+    }
+    case 11:
+    {
+        pch(instruction);
+        break;
+    }
+    case 12:
+    {
+        rch(instruction);
+        break;
+    }
+    case 256:
+    {
+        tracing = 1;
+        break;
+    }
+    case 257:
+    {
+        tracing = 0;
+        break;
+    }
+    }
+}
+
+void pstr(bin_instr_t instruction)
+{
+    GPR[2] = printf("%s", &Memory.words[GPR[4]]);
+}
+
+void pch(bin_instr_t instruction)
+{
+    GPR[2] = fputc(GPR[4], stdout);
+}
+
+void rch(bin_instr_t instruction)
+{
+    GPR[2] = getc(stdin);
+}
+
+void immediateInstructionExecute(bin_instr_t instruction)
+{
+    switch (instruction.immed.op)
+    {
+    case 1:
+        bgez(instruction);
+        break;
+    case 4:
+        beq(instruction);
+        break;
+    case 5:
+        bne(instruction);
+        break;
+    case 6:
+        blez(instruction);
+        break;
+    case 7:
+        bgtz(instruction);
+        break;
+    case 8:
+        bltz(instruction);
+        break;
+    case 9:
+        addi(instruction);
+        break;
+    case 12:
+        andi(instruction);
+        break;
+    case 13:
+        bori(instruction);
+        break;
+    case 14:
+        xori(instruction);
+        break;
+    case 35:
+        lw(instruction);
+        break;
+    case 36:
+        lbu(instruction);
+        break;
+    case 40:
+        sb(instruction);
+        break;
+    case 43:
+        sw(instruction);
+        break;
+    }
+}
+
+void bgez(bin_instr_t instruction)
+{
+    if (GPR[instruction.immed.rs] >= 0)
+    {
+        PC = PC + machine_types_formOffset(instruction.immed.immed);
+    }
+}
+
+void beq(bin_instr_t instruction)
+{
+    if (GPR[instruction.immed.rs] == GPR[instruction.immed.rt])
+    {
+        PC = PC + machine_types_formOffset(instruction.immed.immed);
+    }
+}
+
+void bne(bin_instr_t instruction)
+{
+    if (GPR[instruction.immed.rs] != GPR[instruction.immed.rt])
+    {
+        PC = PC + machine_types_formOffset(instruction.immed.immed);
+    }
+}
+
+void blez(bin_instr_t instruction)
+{
+    if (GPR[instruction.immed.rs] <= 0)
+    {
+        PC = PC + machine_types_formOffset(instruction.immed.immed);
+    }
+}
+
+void bgtz(bin_instr_t instruction)
+{
+    if (GPR[instruction.immed.rs] > 0)
+    {
+        PC = PC + machine_types_formOffset(instruction.immed.immed);
+    }
+}
+
+void bltz(bin_instr_t instruction)
+{
+    if (GPR[instruction.immed.rs] < 0)
+    {
+        PC = PC + machine_types_formOffset(instruction.immed.immed);
+    }
+}
+
+void addi(bin_instr_t instruction)
+{
+    GPR[instruction.immed.rt] = GPR[instruction.immed.rs] + machine_types_sgnExt(instruction.immed.immed);
+}
+
+void andi(bin_instr_t instruction)
+{
+    GPR[instruction.immed.rt] = GPR[instruction.immed.rs] & machine_types_zeroExt(instruction.immed.immed);
+}
+
+void bori(bin_instr_t instruction)
+{
+    GPR[instruction.immed.rt] = GPR[instruction.immed.rs] | machine_types_zeroExt(instruction.immed.immed);
+}
+
+void xori(bin_instr_t instruction)
+{
+    GPR[instruction.immed.rt] = GPR[instruction.immed.rs] ^ machine_types_zeroExt(instruction.immed.immed);
+}
+
+void lw(bin_instr_t instructions)
+{
+    GPR[instructions.immed.rt] = Memory.words[GPR[instructions.immed.rs] + machine_types_formOffset(instructions.immed.immed)];
+}
+
+void lbu(bin_instr_t instruction)
+{
+    GPR[instruction.immed.rt] = machine_types_zeroExt(Memory.bytes[GPR[instruction.immed.rs] + machine_types_formOffset(instruction.immed.immed)]);
+}
+
+void sb(bin_instr_t instructions)
+{
+    Memory.bytes[GPR[instructions.immed.rs] + machine_types_formOffset(instructions.immed.immed)] = GPR[instructions.immed.rt];
+}
+
+void sw(bin_instr_t instructions)
+{
+    Memory.words[GPR[instructions.immed.rs] + machine_types_formOffset(instructions.immed.immed)] = GPR[instructions.immed.rt];
+}
+
+void jumpInstructionExecute(bin_instr_t instruction)
+{
+    switch (instruction.jump.op)
+    {
+    case 2:
+        jmp(instruction);
+        break;
+    case 3:
+        jal(instruction);
+        break;
+    }
+}
+
+void jmp(bin_instr_t instruction)
+{
+    PC = machine_types_formAddress(PC, instruction.jump.addr);
+}
+
+void jal(bin_instr_t instruction)
+{
+    GPR[31] = PC;
+    PC = machine_types_formAddress(PC, instruction.jump.addr);
 }
--- vm_instructions.h
+++ vm_instructions.h
@@ -2,38 +2,3 @@
 #include "machine_types.h"
 
 extern void executeInstruction(bin_instr_t instruction);
-extern void registerInstructionExecute(bin_instr_t instruction);
-extern void add(bin_instr_t instruction);
-extern void sub(bin_instr_t instruction);
-extern void mul(bin_instr_t instruction);
-extern void div(bin_instr_t instruction);
-extern void mfhi(bin_instr_t instruction);
-extern void mflo(bin_instr_t instruction);
-extern void andInstruction(bin_instr_t instruction);
-extern void bor(bin_instr_t instruction);
-extern void nor(bin_instr_t instruction);
-extern void xorInstruction(bin_instr_t instruction);
-extern void jr(bin_instr_t instruction);
-extern void sll(bin_instr_t instruction);
-extern void srl(bin_instr_t instruction);
-
-extern void addi(bin_instr_t instruction);
-extern void andi(bin_instr_t instruction);
-extern void bori(bin_instr_t instruction);
-extern void xori(bin_instr_t instruction);
-extern void beq(bin_instr_t instruction);
-extern void bgez(bin_instr_t instruction);
-extern void bgtz(bin_instr_t instruction);
-extern void blez(bin_instr_t instruction);
-extern void bltz(bin_instr_t instruction);
-extern void bne(bin_instr_t instruction);
-extern void lbu(bin_instr_t instruction);
-extern void lw(bin_instr_t instructions);
-extern void sb(bin_instr_t instructions);
-extern void sw(bin_instr_t instructions);
-
-
-extern void systemcallInstructionExecute(bin_instr_t instruction);
-extern void immediateInstructionExecute(bin_instr_t instruction);
-extern void jumpInstructionExecute(bin_instr_t instruction);
-
